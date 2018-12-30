9f0f96e6b311366219a985e319ee2088935f9c5f
aklyussef <aklyussef@gmail.com>
2018-12-30 21:25:07 +0100

    Added feature scaling for performance testing

--- PatchClassifier.py
+++ PatchClassifier.py
@@ -14,9 +14,11 @@ X = vulndata.drop('Class', axis=1)
 y = vulndata['Class']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+scaled_x_train = preprocessing.scale(X_train)
+scaled_x_test = preprocessing.scale(X_test)
 #svclassifier = SVC(kernel='linear')
 svclassifier = SVC(kernel='rbf', C=100)
-svclassifier.fit(X_train, y_train)
-y_pred = svclassifier.predict(X_test)
+svclassifier.fit(scaled_x_train, y_train)
+y_pred = svclassifier.predict(scaled_x_test)
 print(confusion_matrix(y_test,y_pred))
 print(classification_report(y_test,y_pred))
